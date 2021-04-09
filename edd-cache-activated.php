44946b228a1908ba7c40276f464ea757a4fd3e5c
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2021-04-09 22:17:11 +0000

    v1.0.0

--- edd-cache-activated.php
+++ edd-cache-activated.php
@@ -1,61 +1,13 @@
 <?php
-/**
- * @formatter:off
- * Plugin Name: Easy Digital Downloads - FFWP Modifications
- * Description: Custom additions to EDD for Fast FW Press.
- * Version: 1.5.4
- * Author: Daan (from Fast FW Press)
- * Author URI: https://ffwp.dev
- * Text Domain: ffwp
- * GitHub Plugin URI: https://github.com/Dan0sz/ffwp
- * @formatter:on
- */
 
 /**
- * Define constants.
+ * Plugin Name: Easy Digital Downloads - Cache Plugin Active
+ * Description: A simple plugin that tells EDD that a caching plugin is activated.
+ * Version: 1.0.0
+ * Author: Daan from FFW.Press
+ * Author URI: https://ffw.press
+ * Text Domain: edd-cache-activated
+ * GitHub Plugin URI: https://github.com/Dan0sz/edd-cache-activated
  */
-define('FFWP_PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('FFWP_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('FFWP_PLUGIN_FILE', __FILE__);
 
-/**
- * Takes care of loading classes on demand.
- *
- * @param $class
- *
- * @return mixed|void
- */
-function ffwp_autoload($class)
-{
-    $path = explode('_', $class);
-
-    if ($path[0] != 'FFWP' || (isset($path[1]) && $path[1] == 'Autoloader')) {
-        return;
-    }
-
-    if (!class_exists('FFWP_Autoloader')) {
-        require_once(FFWP_PLUGIN_DIR . 'ffwp-autoload.php');
-    }
-
-    $autoload = new FFWP_Autoloader($class);
-
-    return include FFWP_PLUGIN_DIR . 'includes/' . $autoload->load();
-}
-
-spl_autoload_register('ffwp_autoload');
-
-/**
- * @return FFWP
- */
-function ffwp_init()
-{
-    static $ffwp = null;
-
-    if ($ffwp === null) {
-        $ffwp = new FFWP();
-    }
-
-    return $ffwp;
-}
-
-ffwp_init();
+add_filter('edd_is_caching_plugin_active', __return_true());
